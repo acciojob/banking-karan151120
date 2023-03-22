c1f34c2ce858cfd3c14fb3755e614c94976ad45a
karan151120 <karantulsani28@gmail.com>
2023-03-22 11:39:43 +0530

    22 mar

--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -5,7 +5,11 @@ public class CurrentAccount extends BankAccount{
 
     public CurrentAccount(String name, double balance, String tradeLicenseId) throws Exception {
         // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
+        super(name,balance,5000.0);
 
+        if(balance < 5000) throw new Exception("Insufficient Balance");
+
+        this.tradeLicenseId = tradeLicenseId;
     }
 
     public void validateLicenseId() throws Exception {
@@ -13,7 +17,21 @@ public class CurrentAccount extends BankAccount{
         // If the license Id is valid, do nothing
         // If the characters of the license Id can be rearranged to create any valid license Id
         // If it is not possible, throw "Valid License can not be generated" Exception
+        boolean flag = false;
 
+        for(int i=1; i<tradeLicenseId.length(); i++) {
+            if(tradeLicenseId.charAt(i-1)==tradeLicenseId.charAt(i)) {
+                flag = true;
+                break;
+            }
+        }
+
+        if(!flag) return;
+
+        //function for rearrange
+
+        //not possible
+        throw new Exception("Valid License can not be generated");
     }
 
 }
